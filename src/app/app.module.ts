31aa1b5cd7c8822fe101baf610312306ce410c80
Mairon Gabriel Mariñez Brioso <mmarinez@hidroelectrica.gov.do>
2020-07-29 17:21:50 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+//import { ToastrModule } from 'ngx-toastr';
+//import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //ToastrModule.forRoot(),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interface/data-api.ts
@@ -0,0 +1,8 @@
+export interface DataApi {
+     id: number;
+     FirstName:string;
+     SurName:string;
+     Email:string;
+     UserName:string;
+     Password:string;
+}
